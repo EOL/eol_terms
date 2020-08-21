c8759b04a274583ff2f26c20f0a44db397959249
JRice <jrice@eol.org>
2020-08-21 16:45:03 -0400

    the new file thing coming from git is stupid.

--- lib/eol_terms.rb
+++ lib/eol_terms.rb
@@ -37,77 +37,12 @@ module EolTerms
       @uri_hash
     end
 
-    # Method just runs through all applicable checks, method may be long:
-    # rubocop:disable Metrics/MethodLength
     def validate(silent = false)
-      @seen_uris = {}
-      @problems = []
-      list(true).each_with_index do |term, index|
-        check_duplicate_uris(term, index)
-        check_required_fields(term, index)
-        check_for_illegal_fields(term, index)
-        check_used_for_value(term, index)
-        check_parent_referential_integrity(term, index)
-        check_synonym_referential_integrity(term, index)
-      end
-      report(silent)
-    end
-    # rubocop:enable Metrics/MethodLength
-
-    def check_duplicate_uris(term, index)
-      problem_in_term('Duplicate URI', term, index) if @seen_uris.key?(term['uri'])
-      @seen_uris[term['uri']] = true
-    end
-
-    def check_required_fields(term, index)
-      REQUIRED_FIELDS.each do |field|
-        problem_in_term("Missing required #{field}", term, index) unless property?(term, field)
-      end
-    end
-
-    def check_for_illegal_fields(term, index)
-      term.keys.each do |field|
-        problem_in_term("Illegal field `#{field}`", term, index) unless VALID_FIELDS.include?(field)
-      end
-    end
-
-    def check_used_for_value(term, index)
-      return unless property?(term, 'used_for')
-
-      problem_in_term("Illegal used_for value `#{term['used_for']}`", term, index) unless
-        USED_FOR_VALUES.include?(term['used_for'])
-    end
-
-    def check_parent_referential_integrity(term, index)
-      return unless property?(term, 'parent_uri')
-
-      problem_in_term("Unrecognized URI `#{term['parent_uri']}`", term, index) unless uri_hash(true).key?(term['parent_uri'])
-    end
-
-    def check_synonym_referential_integrity(term, index)
-      return unless property?(term, 'synonym_of_uri')
-
-      problem_in_term("Unrecognized URI `#{term['synonym_of_uri']}`", term, index) unless
-        uri_hash(true).key?(term['synonym_of_uri'])
-    end
-
-    def problem_in_term(problem, term, index)
-      @problems << "#{problem} in term #{index} <#{term['uri']}>"
-    end
-
-    def property?(term, property)
-      # NOTE: this does NOT allow ' ' to be a valid "empty" string. No spaces
-      # allowed.
-      term.key?(property) && !term[property].nil? && !term[property].empty?
-    end
-
-    def report(silent = false)
-      if @problems.any?
-        puts "THERE WERE PROBLEMS:\n#{@problems.join("\n")}" unless silent
-        raise EolTerms::ValidationError, 'Validation failed.'
-      else
-        puts 'Valid.' unless silent
-      end
+      @validator ||= Validator.new
+      @validator.validate(silent)
     end
   end
 end
+
+# It seems weird to put this below the module, but the guide I was following had it here.
+require 'eol_terms/validator'
---
+++ lib/eol_terms/validator.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+# rake build ; rake install ; irb -r eol_terms
+# EolTerms.validate
+module EolTerms
+  # You don't read these.
+  class Validator
+    def initialize
+      @list = EolTerms.list
+      @seen_uris = {}
+      @problems = []
+      @uri_hash = EolTerms.uri_hash(true)
+    end
+
+    # Method just runs through all applicable checks, method may be long:
+    # rubocop:disable Metrics/MethodLength
+    def validate(silent = false)
+      @list(true).each_with_index do |term, index|
+        check_duplicate_uris(term, index)
+        check_required_fields(term, index)
+        check_for_illegal_fields(term, index)
+        check_used_for_value(term, index)
+        check_parent_referential_integrity(term, index)
+        check_synonym_referential_integrity(term, index)
+      end
+      report(silent)
+    end
+    # rubocop:enable Metrics/MethodLength
+
+    def check_duplicate_uris(term, index)
+      problem_in_term('Duplicate URI', term, index) if @seen_uris.key?(term['uri'])
+      @seen_uris[term['uri']] = true
+    end
+
+    def check_required_fields(term, index)
+      REQUIRED_FIELDS.each do |field|
+        problem_in_term("Missing required #{field}", term, index) unless property?(term, field)
+      end
+    end
+
+    def check_for_illegal_fields(term, index)
+      term.keys.each do |field|
+        problem_in_term("Illegal field `#{field}`", term, index) unless VALID_FIELDS.include?(field)
+      end
+    end
+
+    def check_used_for_value(term, index)
+      return unless property?(term, 'used_for')
+
+      problem_in_term("Illegal used_for value `#{term['used_for']}`", term, index) unless
+        USED_FOR_VALUES.include?(term['used_for'])
+    end
+
+    def check_parent_referential_integrity(term, index)
+      return unless property?(term, 'parent_uri')
+
+      problem_in_term("Unrecognized URI `#{term['parent_uri']}`", term, index) unless @uri_hash.key?(term['parent_uri'])
+    end
+
+    def check_synonym_referential_integrity(term, index)
+      return unless property?(term, 'synonym_of_uri')
+
+      problem_in_term("Unrecognized URI `#{term['synonym_of_uri']}`", term, index) unless
+        @uri_hash.key?(term['synonym_of_uri'])
+    end
+
+    def problem_in_term(problem, term, index)
+      @problems << "#{problem} in term #{index} <#{term['uri']}>"
+    end
+
+    def property?(term, property)
+      # NOTE: this does NOT allow ' ' to be a valid "empty" string. No spaces
+      # allowed.
+      term.key?(property) && !term[property].nil? && !term[property].empty?
+    end
+
+    def report(silent = false)
+      if @problems.any?
+        puts "THERE WERE PROBLEMS:\n#{@problems.join("\n")}" unless silent
+        raise EolTerms::ValidationError, 'Validation failed.'
+      else
+        puts 'Valid.' unless silent
+      end
+    end
+  end
+end
