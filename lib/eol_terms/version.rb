2d19f5c694469091ce1f1f8f98f5dee4bfe07504
JRice <jrice@eol.org>
2020-12-17 14:30:03 -0500

    Back to two-digit version numbers

--- lib/eol_terms/version.rb
+++ lib/eol_terms/version.rb
@@ -1,6 +1,3 @@
 module EolTerms
-  # NOTE: Our versions are not "semantic" because we basically just patch EVERY TIME. But we had trouble with using
-  # double-digit patch numbers (bundler wasn't seeing 12 as being updatable from 11), so we're going back to single
-  # digits, even though it means we'll have inappropriate minor and major numbers. Not worth the hassle.
-  VERSION = '0.7.0'.freeze
+  VERSION = '0.7.01'.freeze
 end
